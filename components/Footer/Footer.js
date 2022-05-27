a52b3344126eace065dcafb8d14907bb7c8df739
Riansyah03 <riansyah200102@gmail.com>
2022-05-27 22:28:33 +0700

    first commit

---
+++ components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './footer.module.css'
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.wrapper}>Challenge Chapter 7 - Muhamad Rian Syah</div>
+    </div>
+  )
+}
